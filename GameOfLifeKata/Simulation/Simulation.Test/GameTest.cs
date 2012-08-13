6cec23d076d63abd5a2629072d040103c8a781c6
stardawg <stardawg@ureach.com>
2012-08-13 18:20:11 -0500

    initial commit
    implemented Game constructor and Cells property

---
+++ GameOfLifeKata/Simulation/Simulation.Test/GameTest.cs
@@ -0,0 +1,60 @@
+﻿using Simulation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Simulation.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for GameTest and is intended
+    ///to contain all GameTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class GameTest
+    {
+
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Game Constructor
+        ///</summary>
+        [TestMethod()]
+        public void GameSimulateTest()
+        {
+            Game target = new Game(5, 5);
+
+            Assert.AreEqual(target.Cells.Count, 25);
+        }
+    }
+}
---
+++ GameOfLifeKata/Simulation/Simulation/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simulation
+{
+    public class Game
+    {
+        private int _rows;
+        private int _columns;
+        private object[] _cells;
+
+        public Game(int rows, int columns)
+        {
+            // TODO: Complete member initialization
+            this._rows = rows;
+            this._columns = columns;
+            _cells = new object[rows * columns];
+
+        }
+
+        public IList<object> Cells
+        {
+            get { return _cells.ToList().AsReadOnly(); }
+        }
+    }
+}
